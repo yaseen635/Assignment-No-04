4f87ea898a6538eaa9980e5ba519af6cda602422
yaseen635 <umerooq135@gmail.com>
2025-02-25 02:37:37 -0800

    Add files via upload

---
+++ Assignment No 4/script.js
@@ -0,0 +1,49 @@
+// Chapter 15 (Array I)
+
+// q01
+
+var myArray = [];
+
+// q02
+
+var myStringArray = ["Hello"];
+
+// q03
+
+var alphabet = ["h", "i", "j", "k"];
+alert(alphabet[2]);
+
+// q04
+
+var alphabet = ["h", "i", "j", "k", "l", "m", "n", "o"];
+alert(alphabet.length);
+
+// q05
+
+var myArray = ["First"];
+myArray[1] = "second";
+alert(myArray[1]);
+
+// Chapter 16 (Array II)
+
+// q01
+
+var myArray = ["B.B.Q"];
+myArray.push("Biryani");
+
+// q02
+
+var Alphabet = ["h", "i", "j", "k"];
+Alphabet.pop();
+
+// q03
+
+var Alphabet = ["h", "i", "j", "k"];
+Alphabet.push(10);
+
+// q04
+
+// Chapter 16 (Array III)
+
+// q01
+
